4578279a502e8d0a59ad938ca5a29557d7d3fb35
Bimesh13 <bimesh.shrestha13@gmail.com>
2022-04-29 11:38:29 +0545

    Completes update page design

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+import { EmployeeContext } from "../EmployeeContext";
 
 export default function Update() {
-  return <div>Update</div>;
+  const { employeeData } = useContext(EmployeeContext);
+  return (
+    <>
+      <div className="employee-container">
+        <h1 className="employee-heading">Employee Details</h1>
+        <table className="employee-table">
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Date of Birth</th>
+            <th>Designation</th>
+            <th>Options</th>
+          </tr>
+          {employeeData.map((data) => (
+            <tr className="employee-details" key={data.id}>
+              <td>{data.name}</td>
+              <td>{data.address}</td>
+              <td>{data.dob}</td>
+              <td>{data.designation}</td>
+              <td className="update-buttons">
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </>
+  );
 }
